53c35810a76d2010328a4f0f3b42e90d240f3cfd
george <georgewk1809@gmail.com>
2024-11-20 13:29:45 +0700

    add user model

---
+++ src/Models/User.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(int id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+}
