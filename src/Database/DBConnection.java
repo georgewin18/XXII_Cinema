a923e60f06856598afac2bb7367f6ff0cd5c523f
george <georgewk1809@gmail.com>
2024-11-20 13:33:16 +0700

    hide db creds

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/xxii_cinema";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/...";
+    private static final String USER = "...";
+    private static final String PASSWORD = "...";
 
     public static Connection getConnection() {
         try {
