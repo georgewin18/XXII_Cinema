c33c03f8e9fa7f5a6f1ba0455357e1cf7ceb75a8
george <georgewk1809@gmail.com>
2024-11-20 13:30:13 +0700

    add auth service for user

---
+++ src/Services/AuthService.java
@@ -0,0 +1,35 @@
+package Services;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import Database.DBConnection;
+import Models.User;
+
+public class AuthService {
+    public User authenticate(String username, String password) {
+        String query = "SELECT * FROM users WHERE username = ? AND password = MD5(?)";
+        
+        try (Connection connect = DBConnection.getConnection();
+             PreparedStatement statement = connect.prepareStatement(query)) {
+            
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                return new User(
+                    result.getInt("id"),
+                    result.getString("username"),
+                    result.getString("password"),
+                    result.getString("role")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
